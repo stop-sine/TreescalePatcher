74f7e64e004ab01e87416d70e1609ca3d610eafc
stop-sine <57299283+stop-sine@users.noreply.github.com>
2025-11-17 12:47:28 -0600

    Fix settings tooltop

--- TreescalePatcher/Settings.cs
+++ TreescalePatcher/Settings.cs
@@ -7,7 +7,7 @@ namespace TreescalePatcher
     {
         [SynthesisOrder]
         [SynthesisSettingName("Ignore FormIDs")]
-        [SynthesisTooltip("Provided FormIDs will be ignored. Input must have the format \"FFFFFF|Skyrim.esm\" and omit the leading \"0x\" master index.")]
+        [SynthesisTooltip("Provided FormIDs will be ignored. Input must have the format \"FFFFFF:Skyrim.esm\" and omit the leading \"0x\" master index.")]
         public IEnumerable<FormKey>? Ignored;
     }
 }
